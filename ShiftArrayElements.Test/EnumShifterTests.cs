4f34d55c0c7c6622cb0e9b03668e5dc58544b63a
fabian.pokrzywka <fabian.pokrzywka@users.noreply.gitlab.com>
2022-07-23 15:24:44 +0200

    Change test

--- ShiftArrayElements.Test/EnumShifterTests.cs
+++ ShiftArrayElements.Test/EnumShifterTests.cs
@@ -1,28 +1,27 @@
 ﻿using System;
 using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 namespace ShiftArrayElements.Test
 {
-    [TestClass]
+    [TestFixture]
     public class EnumShifterTests
     {
-        [TestMethod]
+        [Test]
         public void Shift_SourceIsNull_ThrowsArgumentNullException()
         {
-            Assert.ThrowsException<ArgumentNullException>(() => EnumShifter.Shift(null, Array.Empty<Direction>()));
+            Assert.Throws<ArgumentNullException>(() => EnumShifter.Shift(null, Array.Empty<Direction>()));
         }
 
-        [TestMethod]
+        [Test]
         public void Shift_DirectionsIsNull_ThrowsArgumentNullException()
         {
-            Assert.ThrowsException<ArgumentNullException>(() => EnumShifter.Shift(Array.Empty<int>(), null));
+            Assert.Throws<ArgumentNullException>(() => EnumShifter.Shift(Array.Empty<int>(), null));
         }
 
-        [DataRow(new int[] { })]
-        [DataRow(new int[] { 1 })]
-        [DataRow(new int[] { 1, 2, 3, 4, 5})]
-        [TestMethod]
+        [TestCase(new int[] { })]
+        [TestCase(new int[] { 1 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5})]
         public void Shift_SourceIsNotNull_SourceEqualsResult(int[] source)
         {
             int[] result = EnumShifter.Shift(source, Array.Empty<Direction>());
@@ -30,37 +29,35 @@ namespace ShiftArrayElements.Test
             Assert.AreSame(source, result);
         }
 
-        [DataRow(new object[] { -1 })]
-        [DataRow(new object[] { Direction.Left, 2})]
-        [DataRow(new object[] { Direction.Left, Direction.Right, 3})]
-        [TestMethod]
+        [TestCase(new object[] { -1 })]
+        [TestCase(new object[] { Direction.Left, 2})]
+        [TestCase(new object[] { Direction.Left, Direction.Right, 3})]
         public void Shift_DirectionsContainsWrongValues_ThrowsInvalidOperationException(object[] objects)
         {
             var directions = objects.Cast<Direction>().ToArray();
 
-            Assert.ThrowsException<InvalidOperationException>(() => EnumShifter.Shift(new[] { 1, 2, 3, 4, 5 }, directions));
+            Assert.Throws<InvalidOperationException>(() => EnumShifter.Shift(new[] { 1, 2, 3, 4, 5 }, directions));
         }
 
-        [DataRow(new[] { 1 }, new Direction[] { }, new[] { 1 })]
-        [DataRow(new[] { 1 }, new[] { Direction.Left }, new[] { 1 })]
-        [DataRow(new[] { 1 }, new[] { Direction.Right }, new[] { 1 })]
-        [DataRow(new[] { 1, 2 }, new Direction[] { }, new[] { 1, 2 })]
-        [DataRow(new[] { 1, 2 }, new[] { Direction.Left }, new[] { 2, 1 })]
-        [DataRow(new[] { 1, 2 }, new[] { Direction.Right }, new[] { 2, 1 })]
-        [DataRow(new[] { 1, 2 }, new[] { Direction.Left, Direction.Left, Direction.Right }, new[] { 2, 1 })]
-        [DataRow(new[] { 1, 2 }, new[] { Direction.Right, Direction.Right, Direction.Left }, new[] { 2, 1 })]
-        [DataRow(new[] { 1, 2, 3 }, new Direction[] { }, new[] { 1, 2, 3 })]
-        [DataRow(new[] { 1, 2, 3 }, new[] { Direction.Left, Direction.Right, Direction.Left, Direction.Right }, new[] { 1, 2, 3 })]
-        [DataRow(new[] { 1, 2, 3 }, new[] { Direction.Right, Direction.Left, Direction.Right, Direction.Left }, new[] { 1, 2, 3 })]
-        [DataRow(new[] { 1, 2, 3 }, new[] { Direction.Left, Direction.Left, Direction.Right, Direction.Right }, new[] { 1, 2, 3 })]
-        [DataRow(new[] { 1, 2, 3 }, new[] { Direction.Right, Direction.Right, Direction.Left, Direction.Left }, new[] { 1, 2, 3 })]
-        [DataRow(new[] { 1, 2, 3, 4, 5 }, new[] { Direction.Left }, new[] { 2, 3, 4, 5, 1 })]
-        [DataRow(new[] { 1, 2, 3, 4, 5 }, new[] { Direction.Right }, new[] { 5, 1, 2, 3, 4 })]
-        [DataRow(new[] { 1, 2, 3, 4, 5 }, new[] { Direction.Left, Direction.Left, Direction.Left, Direction.Left, Direction.Left }, new[] { 1, 2, 3, 4, 5 })]
-        [DataRow(new[] { 1, 2, 3, 4, 5 }, new[] { Direction.Right, Direction.Right, Direction.Right, Direction.Right, Direction.Right }, new[] { 1, 2, 3, 4, 5 })]
-        [DataRow(new[] { 1, 2, 3, 4, 5 }, new[] { Direction.Left, Direction.Left, Direction.Left, Direction.Left, Direction.Left }, new[] { 1, 2, 3, 4, 5 })]
-        [DataRow(new[] { 1, 2, 3, 4, 5 }, new[] { Direction.Right, Direction.Right, Direction.Right, Direction.Right, Direction.Right }, new[] { 1, 2, 3, 4, 5 })]
-        [TestMethod]
+        [TestCase(new[] { 1 }, new Direction[] { }, new[] { 1 })]
+        [TestCase(new[] { 1 }, new[] { Direction.Left }, new[] { 1 })]
+        [TestCase(new[] { 1 }, new[] { Direction.Right }, new[] { 1 })]
+        [TestCase(new[] { 1, 2 }, new Direction[] { }, new[] { 1, 2 })]
+        [TestCase(new[] { 1, 2 }, new[] { Direction.Left }, new[] { 2, 1 })]
+        [TestCase(new[] { 1, 2 }, new[] { Direction.Right }, new[] { 2, 1 })]
+        [TestCase(new[] { 1, 2 }, new[] { Direction.Left, Direction.Left, Direction.Right }, new[] { 2, 1 })]
+        [TestCase(new[] { 1, 2 }, new[] { Direction.Right, Direction.Right, Direction.Left }, new[] { 2, 1 })]
+        [TestCase(new[] { 1, 2, 3 }, new Direction[] { }, new[] { 1, 2, 3 })]
+        [TestCase(new[] { 1, 2, 3 }, new[] { Direction.Left, Direction.Right, Direction.Left, Direction.Right }, new[] { 1, 2, 3 })]
+        [TestCase(new[] { 1, 2, 3 }, new[] { Direction.Right, Direction.Left, Direction.Right, Direction.Left }, new[] { 1, 2, 3 })]
+        [TestCase(new[] { 1, 2, 3 }, new[] { Direction.Left, Direction.Left, Direction.Right, Direction.Right }, new[] { 1, 2, 3 })]
+        [TestCase(new[] { 1, 2, 3 }, new[] { Direction.Right, Direction.Right, Direction.Left, Direction.Left }, new[] { 1, 2, 3 })]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, new[] { Direction.Left }, new[] { 2, 3, 4, 5, 1 })]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, new[] { Direction.Right }, new[] { 5, 1, 2, 3, 4 })]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, new[] { Direction.Left, Direction.Left, Direction.Left, Direction.Left, Direction.Left }, new[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, new[] { Direction.Right, Direction.Right, Direction.Right, Direction.Right, Direction.Right }, new[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, new[] { Direction.Left, Direction.Left, Direction.Left, Direction.Left, Direction.Left }, new[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, new[] { Direction.Right, Direction.Right, Direction.Right, Direction.Right, Direction.Right }, new[] { 1, 2, 3, 4, 5 })]
         public void Shift_SourceAndDirectionsAreNotNull_ReturnsArrayWithShiftedElements(int[] source, Direction[] directions, int[] expected)
         {
             var actual = EnumShifter.Shift(source, directions);
--- ShiftArrayElements.Test/ShifterTests.cs
+++ ShiftArrayElements.Test/ShifterTests.cs
@@ -1,47 +1,44 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
 using System.Linq;
+using NUnit.Framework;
 
 namespace ShiftArrayElements.Test
 {
-    [TestClass]
+    [TestFixture]
     public class ShifterTests
     {
-        [TestMethod]
+        [Test]
         public void Shift_SourceIsNull_ThrowsArgumentNullException()
         {
-            Assert.ThrowsException<ArgumentNullException>(() => Shifter.Shift(null, Array.Empty<int>()));
+            Assert.Throws<ArgumentNullException>(() => Shifter.Shift(null, Array.Empty<int>()));
         }
 
-        [TestMethod]
+        [Test]
         public void Shift_IterationsIsNull_ThrowsArgumentNullException()
         {
-            Assert.ThrowsException<ArgumentNullException>(() => Shifter.Shift(Array.Empty<int>(), null));
+            Assert.Throws<ArgumentNullException>(() => Shifter.Shift(Array.Empty<int>(), null));
         }
-
-        [TestMethod]
-        [DataRow(new int[] { })]
-        [DataRow(new int[] { 1 })]
-        [DataRow(new int[] { 1, 2, 3, 4, 5 })]
+        
+        [TestCase(new int[] { })]
+        [TestCase(new int[] { 1 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 })]
         public void Shift_SourceIsNotNull_SourceEqualsResult(int[] source)
         {
             int[] result = Shifter.Shift(source, Array.Empty<int>());
 
             Assert.AreSame(source, result);
         }
-
-
-        [DataRow(new int[] { }, new int[] { 0 }, new int[] { })]
-        [DataRow(new int[] { }, new int[] { }, new int[] { })]
-        [DataRow(new[] { 1 }, new[] { 0 }, new[] { 1 })]
-        [DataRow(new[] { 1 }, new[] { 1 }, new[] { 1 })]
-        [DataRow(new[] { 1, 2 }, new[] { 1 }, new[] { 2, 1 })]
-        [DataRow(new[] { 1, 2 }, new[] { 2 }, new[] { 1, 2 })]
-        [DataRow(new[] { 1, 2 }, new[] { 1, 1 }, new[] { 1, 2 })]
-        [DataRow(new[] { 1, 2 }, new[] { 1, 2 }, new[] { 2, 1 })]
-        [DataRow(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 2, 3 }, new[] { 3, 4, 5, 1, 2 })]
-        [DataRow(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 2, 3, 4 }, new[] { 4, 5, 1, 2, 3 })]
-        [TestMethod]
+        
+        [TestCase(new int[] { }, new int[] { 0 }, new int[] { })]
+        [TestCase(new int[] { }, new int[] { }, new int[] { })]
+        [TestCase(new[] { 1 }, new[] { 0 }, new[] { 1 })]
+        [TestCase(new[] { 1 }, new[] { 1 }, new[] { 1 })]
+        [TestCase(new[] { 1, 2 }, new[] { 1 }, new[] { 2, 1 })]
+        [TestCase(new[] { 1, 2 }, new[] { 2 }, new[] { 1, 2 })]
+        [TestCase(new[] { 1, 2 }, new[] { 1, 1 }, new[] { 1, 2 })]
+        [TestCase(new[] { 1, 2 }, new[] { 1, 2 }, new[] { 2, 1 })]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 2, 3 }, new[] { 3, 4, 5, 1, 2 })]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 2, 3, 4 }, new[] { 4, 5, 1, 2, 3 })]
         public void Shift_SourceAndIterationsAreNotNull_ReturnArraysWithShiftedElements(int[] source, int[] iterations, int[] expected)
         {
             var actual = Shifter.Shift(source, iterations);
